cbd5b8653533930a337f9cdc32a6f9764811db52
cit666-hhh <2021205608@qq.com>
2023-09-27 22:49:17 +0800

    okk

--- src/animal/Animal.java
+++ src/animal/Animal.java
@@ -1,6 +1,28 @@
-public class Animal {
+package animal;
 
-    int age;String name;boolean isMale;double cost;\
+public  abstract class  Animal {
 
 
+
+    int age;
+    String name;
+    boolean isMale;
+    double cost;
+
+    public Animal(int age, String name, boolean isMale, double cost) {
+        this.age = age;
+        this.name = name;
+        this.isMale = isMale;
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", isMale=" + isMale +
+                ", cost=" + cost +
+                '}';
+    }
 }
--- src/animal/InsufficientBalanceException.java
+++ src/animal/InsufficientBalanceException.java
@@ -1,11 +1,11 @@
-package animal;
+package animal;//这里是按照任务定义的一个自定义异常，就是宠物店想要批发，但是没钱了
 
-public class OutOfMoneyException extends RuntimeException{
+public class InsufficientBalanceException extends RuntimeException{
 
-    public OutOfMoneyException() {
+    public InsufficientBalanceException() {
     }
 
-    public OutOfMoneyException(String message) {
+    public InsufficientBalanceException(String message) {
         super(message);
     }
 }
